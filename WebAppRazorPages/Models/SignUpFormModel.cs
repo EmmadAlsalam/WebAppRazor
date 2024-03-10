f6048f96a6520a67ec9366b03f9b3749123d1512
EmmadAlsalam <143703288+EmmadAlsalam@users.noreply.github.com>
2024-03-10 16:26:45 +0100

    Signin and Signut completed

--- WebAppRazorPages/Models/SignUpFormModel.cs
+++ WebAppRazorPages/Models/SignUpFormModel.cs
@@ -8,19 +8,23 @@ public class SignUpFormModel
 {
     [DisplayName("First name")]
     [Required(ErrorMessage = "Invalid first name")]
+    [MinLength(2,ErrorMessage ="Enter a first name")]
     public string FirstName { get; set; } = null!;
 
     [DisplayName("Lats name")]
     [Required(ErrorMessage = "Invalid Last name")]
+    [MinLength(2, ErrorMessage = "Enter a LastName")]
     public string LastName { get; set; } = null!;
 
     [DisplayName("Email adress")]
     [Required(ErrorMessage = "Invalid Email adress")]
+    [RegularExpression("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Invalid email")]
     public string Email { get; set; } = null!;
 
     [DataType(DataType.Password)]
     [DisplayName("Password")]
     [Required(ErrorMessage = "Invalid Password")]
+
     public string Password { get; set; } = null!;
     [DataType(DataType.Password)]
 
---
+++ WebAppRazorPages/Pages/Shared/_ValidationScript.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAppRazorPages.Pages.Shared
+{
+    public class _ValidationScriptModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
